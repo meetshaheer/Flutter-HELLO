02fc26d06445a7415a43852a4a20f5fc4e0bddbb
Shaheer Ahmed <shaheer.ahmed0404@gmail.com>
2024-03-12 23:59:40 +0500

    creating project and import stl class

---
+++ flutter_application/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application/practice.dart';
+
+void main(List<String> args) {}
+
+class myApp extends StatelessWidget {
+  const myApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: practice(),
+    );
+  }
+}
