c7eee890a83e6f581d5728283ccbf24f276292f4
Shaheer Ahmed <shaheer.ahmed0404@gmail.com>
2024-03-13 01:14:27 +0500

    just completed the HELLO using columns, rows and containers

--- flutter_application/lib/practice.dart
+++ flutter_application/lib/practice.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 class practice extends StatelessWidget {
@@ -7,107 +9,415 @@ class practice extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Box Hello DM DM "),
+        title: const Text("Box Hello DM DM "),
       ),
-      body: Container(
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Row(
-              children: [
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.amber[50],
-                ),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.white,
-                ),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.amber[400],
-                ),
-              ],
+            Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        height: 100,
+                        width: 100,
+                        color: Colors.amber[50],
+                      ),
+                      Container(
+                        height: 100,
+                        width: 100,
+                        color: Colors.white,
+                      ),
+                      Container(
+                        height: 100,
+                        width: 100,
+                        color: Colors.amber[400],
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        height: 100,
+                        width: 100,
+                        color: Colors.amber[100],
+                      ),
+                      Container(
+                        height: 100,
+                        width: 100,
+                        color: Colors.white,
+                      ),
+                      Container(
+                        height: 100,
+                        width: 100,
+                        color: Colors.amber[300],
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        height: 100,
+                        width: 100,
+                        color: Colors.amber[200],
+                      ),
+                      Container(
+                        height: 100,
+                        width: 100,
+                        color: Colors.amber[300],
+                      ),
+                      Container(
+                        height: 100,
+                        width: 100,
+                        color: Colors.amber[200],
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        height: 100,
+                        width: 100,
+                        color: Colors.amber[300],
+                      ),
+                      Container(
+                        height: 100,
+                        width: 100,
+                        color: Colors.white,
+                      ),
+                      Container(
+                        height: 100,
+                        width: 100,
+                        color: Colors.amber[100],
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        height: 100,
+                        width: 100,
+                        color: Colors.amber[400],
+                      ),
+                      Container(
+                        height: 100,
+                        width: 100,
+                        color: Colors.white,
+                      ),
+                      Container(
+                        height: 100,
+                        width: 100,
+                        color: Colors.amber[50],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-            Row(
-              children: [
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.amber[100],
-                ),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.white,
-                ),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.amber[300],
-                ),
-              ],
+            SizedBox(height: 200),
+            Container(
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                          width: 100, height: 100, color: Colors.purple[50]),
+                      Container(
+                        width: 100,
+                        height: 100,
+                        color: Colors.purple[100],
+                      ),
+                      Container(
+                        width: 100,
+                        height: 100,
+                        color: Colors.purple[200],
+                      ),
+                    ],
+                  ),
+                  Row(children: [
+                    Container(
+                      width: 100,
+                      height: 100,
+                      color: Colors.purple[100],
+                    ),
+                  ]),
+                  Row(
+                    children: [
+                      Container(
+                        width: 100,
+                        height: 100,
+                        color: Colors.purple[200],
+                      ),
+                      Container(
+                        width: 100,
+                        height: 100,
+                        color: Colors.purple[100],
+                      ),
+                      Container(
+                        width: 100,
+                        height: 100,
+                        color: Colors.purple[50],
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        width: 100,
+                        height: 100,
+                        color: Colors.purple[100],
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        width: 100,
+                        height: 100,
+                        color: Colors.purple[200],
+                      ),
+                      Container(
+                        width: 100,
+                        height: 100,
+                        color: Colors.purple[50],
+                      ),
+                      Container(
+                        width: 100,
+                        height: 100,
+                        color: Colors.purple[100],
+                      ),
+                    ],
+                  )
+                ],
+              ),
             ),
-            Row(
-              children: [
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.amber[200],
-                ),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.amber[300],
-                ),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.amber[200],
-                ),
-              ],
+            SizedBox(height: 200),
+            Container(
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        width: 100,
+                        height: 100,
+                        color: Colors.lightBlue[100],
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        width: 100,
+                        height: 100,
+                        color: Colors.lightBlue[200],
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        width: 100,
+                        height: 100,
+                        color: Colors.lightBlue[300],
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        width: 100,
+                        height: 100,
+                        color: Colors.lightBlue[400],
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        width: 100,
+                        height: 100,
+                        color: Colors.lightBlue[600],
+                      ),
+                      Container(
+                        width: 100,
+                        height: 100,
+                        color: Colors.lightBlue[700],
+                      ),
+                      Container(
+                        width: 100,
+                        height: 100,
+                        color: Colors.lightBlue[800],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-            Row(
-              children: [
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.amber[300],
-                ),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.white,
-                ),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.amber[100],
-                ),
-              ],
+            SizedBox(height: 200),
+            Container(
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        width: 100,
+                        height: 100,
+                        color: Colors.lightBlue[100],
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        width: 100,
+                        height: 100,
+                        color: Colors.lightBlue[200],
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        width: 100,
+                        height: 100,
+                        color: Colors.lightBlue[300],
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        width: 100,
+                        height: 100,
+                        color: Colors.lightBlue[400],
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        width: 100,
+                        height: 100,
+                        color: Colors.lightBlue[600],
+                      ),
+                      Container(
+                        width: 100,
+                        height: 100,
+                        color: Colors.lightBlue[700],
+                      ),
+                      Container(
+                        width: 100,
+                        height: 100,
+                        color: Colors.lightBlue[800],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-            Row(
-              children: [
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.amber[400],
-                ),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.white,
-                ),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.amber[50],
-                ),
-              ],
-            ),
+            SizedBox(height: 200),
+            Container(
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        height: 100,
+                        width: 100,
+                        color: Colors.lime[50],
+                      ),
+                      Container(
+                        height: 100,
+                        width: 100,
+                        color: Colors.lime[100],
+                      ),
+                      Container(
+                        height: 100,
+                        width: 100,
+                        color: Colors.lime[200],
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        height: 100,
+                        width: 100,
+                        color: Colors.lime[300],
+                      ),
+                      Container(
+                        height: 100,
+                        width: 100,
+                      ),
+                      Container(
+                        height: 100,
+                        width: 100,
+                        color: Colors.lime[300],
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        height: 100,
+                        width: 100,
+                        color: Colors.lime[400],
+                      ),
+                      Container(
+                        height: 100,
+                        width: 100,
+                      ),
+                      Container(
+                        height: 100,
+                        width: 100,
+                        color: Colors.lime[400],
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        height: 100,
+                        width: 100,
+                        color: Colors.lime[500],
+                      ),
+                      Container(
+                        height: 100,
+                        width: 100,
+                      ),
+                      Container(
+                        height: 100,
+                        width: 100,
+                        color: Colors.lime[500],
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        height: 100,
+                        width: 100,
+                        color: Colors.lime[600],
+                      ),
+                      Container(
+                        height: 100,
+                        width: 100,
+                        color: Colors.lime[700],
+                      ),
+                      Container(
+                        height: 100,
+                        width: 100,
+                        color: Colors.lime[800],
+                      )
+                    ],
+                  )
+                ],
+              ),
+            )
           ],
         ),
       ),
